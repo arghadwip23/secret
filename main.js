a3687f3ffa90b35a502eaf6a79efd568e536a901
Arghadwip Sarkar <68644790+arghadwip23@users.noreply.github.com>
2021-05-26 18:29:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -52,8 +52,8 @@ navigator.mediaDevices.getUserMedia(constraints)
         // console.log(data);
     }).catch(()=>{
       document.getElementById('lt').innerText='Permission Required';
-     document.getElementById('lt').style.font-Size 
-    }); = "1 rem" ;
+     document.getElementById('lt').style.fontSize="0.75em";
+    });  
     
 function main() {
    captureButton.style.display = 'none';
